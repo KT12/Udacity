81487160243a60cea743593ff57b02bcc0c28b37
KT12 <nyy2008@gmail.com>
2017-02-06 17:48:27 -0500

    Additional practice

--- ud_370/convolutions.py
+++ ud_370/convolutions.py
@@ -95,3 +95,38 @@ graph = tf.Graph()
 			hidden_3 = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
 
 			return tf.matmul(hidden_3, layer4_weights) + layer4_biases
+
+		# Train computation
+		logits = model(tf_train_dataset)
+		loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+				labels=tf_train_labels, logits=logits))
+
+		# Optimizer
+		optimizer = tf.train.GradienDescentOptimizer(0.05).minimize(loss)
+
+
+		# Predictions
+		train_prediction = tf.nn.softmax(logits)
+		valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+		test_prediction  = tf.nn.softmax(model(tf_test_dataset))
+
+# Run the covnolutional neural network
+
+num_steps = 1001
+
+with tf.Session(graph=graph) as sesssion:
+	tf.initialize_all_variables().run()
+	print('Initialized')
+	for step in range(num_steps):
+		offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+		batch_data = train_dataset[offset:(offset + batch_size),:]
+		batch_labels = train_labels[offset:(offset + batch_size),:]
+		feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+		_, l, predictions = session.run([optimizer, loss, train_prediction], 
+				feed_dict=feed_dict)
+		if (step % 100 == 0):
+			print('Minibatch loss at step %d: %f' % (step, l))
+      		print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+      		print('Validation accuracy: %.1f%%' % accuracy(
+        		valid_prediction.eval(), valid_labels))
+	print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
