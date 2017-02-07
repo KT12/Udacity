8ce401815140c33605df6f8ba530881ca96bf21c
KT12 <nyy2008@gmail.com>
2017-02-07 14:46:27 -0500

    Save work

--- ud_370/convolutions.py
+++ ud_370/convolutions.py
@@ -219,3 +219,45 @@ with graph.as_default():
             		print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
             		print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
     	print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+
+
+# With dropout regularization
+
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 64
+alpha = 0.15
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+	tf.set_random_seed(1)
+
+	# Input data
+
+	tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size,
+		image_size, num_channels))
+	tf_train_labels  = tf.placeholder(tf.float32, shape+(batch_size, num_labels))
+	tf_valid_dataset = tf.constant(valid_dataset)
+	tf_test_dataset  = tf.constant(test_dataset)
+
+	# Decay learning rate
+	global_step = tf.Variable(0, trainable=False)
+	learning_rate = tf.train.exponential_decay(alpha, global_step, 100, 0.95,
+		staircase=True)
+
+	# Weights
+	layer1_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, num_channels, depth], stddev=0.1))
+    layer1_biases  = tf.Variable(tf.zeros([depth]))
+    
+    layer2_weights = tf.Variable(tf.truncated_normal([patch_size, patch_size, depth, depth], stddev=0.1))
+    layer2_biases  = tf.Variable(tf.constant(1.0, shape=[depth]))
+
+    layer3_weights = tf.Variable(tf.truncated_normal([image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
+    layer3_biases  = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+  
+    layer4_weights = tf.Variable(tf.truncated_normal([num_hidden, num_labels], stddev=0.1))
+    layer4_biases  = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+  
