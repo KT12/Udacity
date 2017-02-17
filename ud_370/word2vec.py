e823fd255ff1f1129521f87bc912705a5df40694
KT12 <nyy2008@gmail.com>
2017-02-16 19:38:49 -0500

    Grok more

--- ud_370/word2vec.py
+++ ud_370/word2vec.py
@@ -62,3 +62,29 @@ data, count, dictionary, reverse_dictionary = build_dataset(words)
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10])
 del words # Reduce memory
+
+data_index = 0
+
+def generate_batch(batch_size, num_skips, skip_window):
+	global data_index
+	assert batch_size % num_skips == 0
+	assert num_skips <= 2 * skip_window
+	batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+	labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+	span = 2 * skip_window + 1 # skipw, target, skipw
+	buffer = collections.deque(maxlen=span)
+	for _ in range(span):
+		buffer.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+	for i in range(batch_size // num_skips):
+		target = skip_window # target label at the center of the buffer
+		targets_to_avoid = [skip_window]
+		for j in range(num_skips):
+			while target in targets_to_avoid:
+				target = random.randint(0, span - 1)
+			targets_to_avoid.append(target)
+			batch[i * num_skips + j] = buffer[skip_window]
+			labels[i * num_skips + j, 0] = buffer[target]
+		buffer.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+	return batch, labels
