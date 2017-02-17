cd99d66fc720515f3c2a285acb572a5d4260b677
KT12 <nyy2008@gmail.com>
2017-02-17 15:21:45 -0500

    More code

--- ud_370/word2vec.py
+++ ud_370/word2vec.py
@@ -185,4 +185,24 @@ with tf.Session(graph=graph) as session:
 					close_word = reverse_dictionary[nearest[k]]
 					log = '%s %s,' % (log, close_word)
 				print(log)
-	final_embeddings = normalized_embeddings.eval()			
+	final_embeddings = normalized_embeddings.eval()
+
+num_points = 400
+
+tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :])
+
+def plot(embeddings, labels):
+	assert embeddings.shape[0] >= len(labels, 'More labels than embeddings')
+	pylab.figure(figsize=(15,15)) # in inches
+	for i, label in enumerate(labels):
+		x, y = embeddings[i,:]
+		pylab.scatter(x, y)
+		pylab.annotate(label, xy=(x, y), xytext=(5,2), textcoords='offset points',
+			ha='right', va='bottom')
+	pylab.show()
+
+words = [reverse_dictionary[i] for i in range(1, num_points + 1)]
+plot(two_d_embeddings, words)
+
+
