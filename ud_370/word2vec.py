162bf8ef7fa441fe33012edbce6f259c5e789e58
KT12 <nyy2008@gmail.com>
2017-02-16 21:34:22 -0500

    Code more

--- ud_370/word2vec.py
+++ ud_370/word2vec.py
@@ -88,3 +88,101 @@ def generate_batch(batch_size, num_skips, skip_window):
 		buffer.append(data[data_index])
 		data_index = (data_index + 1) % len(data)
 	return batch, labels
+
+print('data:', [reverse_dictionary[di] for di in data[:8]])
+
+for num_skips, skip_window in [(2, 1), (4,2)]:
+	data_index = 0
+	batch, labels = generate_batch(batch_size=8, num_skips=num_skips, 
+		skip_window=skip_window)
+	print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
+	print('    batch:', [reverse_dictionary[bi] for bi in batch])
+	print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
+
+
+batch_size = 128
+embedding_size = 128 # dim of embedding vector
+skip_window = 1 #words on either side
+num_skips = 2 # how many times to reuse input to generate label
+
+# We pick a random validation set to sample nearest neighbors. here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent.
+
+valid_size = 16 # random set of words to evaluate similarity on
+valid_window = 100 # only pick devs samples in head of distro
+valid_examples = np.array(random.sample(range(valid_window), valid_size))
+
+graph = tf.Graph()
+
+with graph.as_default(), tf.device('/cpu:0'):
+
+	# Random seed
+	tf.set_random_seed(1)
+
+	# Input data
+	train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
+	train_labels  = tf.placeholder(tf.int32, shape=[batch_size, 1])
+	valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+	# Variables
+	embeddings = tf.Variable(
+		tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+	softmax_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size],
+		stddev=1.0 / math.sqrt(embedding_size)))
+	softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+	# Model
+	# Look up embeddings for inputs
+	embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+	# Compute the softmax loss, using a sample of the negative labels each time.
+	loss = tf.reduce_mean(tf.nn.sampled_softmax_loss(weights=softmax_weights, 
+		biases=softmax_biases, inputs=embed, labels=train_labels,
+		num_sampled=num_sampled, num_classes=vocabulary_size))
+
+    # Optimizer.
+    # Note: The optimizer will optimize the softmax_weights AND the embeddings.
+    # This is because the embeddings are defined as a variable quantity and the
+    # optimizer's `minimize` method will by default modify all variable quantities 
+    # that contribute to the tensor it is passed.
+    # See docs on `tf.train.Optimizer.minimize()` for more details.
+    optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+
+    # Compute the similarity between minibatch examples and all embeddings.
+    # We use the cosine distance:
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+    normalized_embeddings = embeddings / norm
+    valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+    similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+
+num_steps = 100001
+
+with tf.Session(graph=graph) as session:
+	tf.initialize_all_variables().run()
+	print('Initialized')
+	average_loss = 0
+
+	for step in range(num_steps):
+		batch_data, batch_labels = generate_batch(batch_size, num_skips, skip_window)
+		feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
+		_, l = session.run([optimizer, loss], feed_dict=feed_dict)
+		average_loss += l
+		if step % 2000 == 0:
+			if step > 0:
+				average_loss = average_loss / 2000
+			# avg loss is estimate of loss over last 2000 batches
+			print('Average loss at step %d : %f' % (step, average_loss))
+			average_loss = 0
+		# note that this is expensive (~20% slowdown if computed every 500 steps)
+		if step % 10000 == 0:
+			sim = similarity.eval()
+			for i in range(valid_size):
+				valid_word = reverse_dictionary[valid_examples[i]]
+				top_k = 8 # num of nearest neighbors
+				nearest = (-sim[i, :]).argsort()[1:top_k+1]
+				log = 'Nearest to %s:' % valid_word
+				for k in range(top_k):
+					close_word = reverse_dictionary[nearest[k]]
+					log = '%s %s,' % (log, close_word)
+				print(log)
+	final_embeddings = normalized_embeddings.eval()			
