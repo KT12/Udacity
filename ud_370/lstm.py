2da913c7be3246f1728494e9ac41d3c011b4590b
KT12 <nyy2008@gmail.com>
2017-02-19 11:55:48 -0500

    Grok LSTM

--- ud_370/lstm.py
+++ ud_370/lstm.py
@@ -25,3 +25,42 @@ def maybe_download(filename, expected_bytes):
 
 filename = maybe_download('text8.zip', 31344016)
 
+def read_data(filename):
+	f = zipfile.ZipFile(filename)
+	for name in f.namelist():
+		return tf.compat.as_str(f.read(name))
+	f.close()
+
+text = read_data(filename)
+print('Data size %d' % len(text))
+
+# Create validation set
+valid_size = 1000
+valid_text = text[:valid_size]
+train_text = text[valid_size:]
+train_size = len(train_text)
+print(train_size, train_text[:64])
+print(valid_size, valid_text[:64])
+
+# Utility functions to map char to vocab id's and back
+
+vocabulary_size = len(string.ascii_lowercase) + 1 # a-z + ' '
+first_letter = ord(string.ascii_lowercase[0])
+
+def char2id(char):
+	if char in string.ascii_lowercase:
+		return ord(char) - first_letter + 1
+	elif char == ' ':
+		return 0
+	else:
+		print('Unexpected character: %s' % char)
+		return 0
+
+def id2char(dictid):
+	if dictid > 0:
+		return chr(dictid + first_letter - 1)
+	else:
+		return ' '
+
+print(char2id('a'), char2id('z'), char2id(' '), char2id('ï'))
+print(id2char(1), id2char(26), id2char(0))
