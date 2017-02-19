543ba5b1010c38c8c8b4514b516875d731a01129
KT12 <nyy2008@gmail.com>
2017-02-19 16:31:28 -0500

    More LSTM

--- ud_370/lstm.py
+++ ud_370/lstm.py
@@ -64,3 +64,26 @@ def id2char(dictid):
 
 print(char2id('a'), char2id('z'), char2id(' '), char2id('ï'))
 print(id2char(1), id2char(26), id2char(0))
+
+# Function to generate a training batch for the LSTM model
+
+batch_size = 64
+num_unrollings = 10
+
+class BatchGenerator(object):
+	def __init__(self, text, batch_size, num_unrollings):
+		self._text = text
+		self._text_size = len(text)
+		self._batch_size = batch_size
+		self._num_unrollings = num_unrollings
+		segment = self._text_size // batch_size
+		self._cursor = [offset * segment for offset in range(batch_size)]
+		self._last_batch = self._next_batch()
+
+	def _next_batch(self):
+		# Generate a single batch from the current cursor position in the data
+		batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
+		for b in range(self._batch_size):
+			batch[b, char2id(self._text[self._cursor[b]])] = 1.0
+			self._cursor[b] = (self._cursor[b] + 1) % self._text_size
+		return batch
